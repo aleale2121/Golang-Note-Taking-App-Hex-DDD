c49abe416fdb73ebbc787ea26671e4befca20d58
aleale2121 <alefewyimer4@gmail.com>
2021-03-08 09:17:50 +0300

    implement delete note service

--- pkg/deleting/service.go
+++ pkg/deleting/service.go
@@ -1,1 +1,28 @@
 package deleting
+
+import "fmt"
+
+var ErrDuplicate = fmt.Errorf("title already exist")
+
+type Service interface {
+	DeleteNote(uint) error
+}
+
+type Repository interface {
+	DeleteNote(uint) error
+}
+
+type service struct {
+	tR Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+func (s *service) DeleteNote(noteID uint) error {
+	if noteID == 0 {
+		return fmt.Errorf("invalid note id")
+	}
+	return s.tR.DeleteNote(noteID)
+}
