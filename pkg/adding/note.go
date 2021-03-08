22919e9a239b8075ecae06ba7287751775f22781
aleale2121 <alefewyimer4@gmail.com>
2021-03-08 09:17:27 +0300

    implement add note service and repository

---
+++ pkg/adding/note.go
@@ -0,0 +1,6 @@
+package adding
+
+type Note struct {
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
--- pkg/adding/service.go
+++ pkg/adding/service.go
@@ -1,1 +1,28 @@
 package adding
+
+import "fmt"
+
+var ErrDuplicate = fmt.Errorf("title already exist")
+
+type Service interface {
+	AddNote(Note) error
+}
+
+type Repository interface {
+	AddNote(Note) error
+}
+
+type service struct {
+	tR Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{r}
+}
+
+func (s *service) AddNote(note Note) error {
+	if note.Title == "" || note.Detail == "" {
+		return fmt.Errorf("invalid input")
+	}
+	return s.tR.AddNote(note)
+}
