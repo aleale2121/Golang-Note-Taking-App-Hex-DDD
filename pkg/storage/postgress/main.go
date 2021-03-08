d98411377aaf75b0707e6f5116e1081bce239cb0
aleale2121 <alefewyimer4@gmail.com>
2021-03-08 11:22:11 +0300

    creating method for accessing postgress db

--- pkg/storage/postgress/main.go
+++ pkg/storage/postgress/main.go
@@ -1,1 +1,17 @@
 package postgress
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+func GetDatabaseConnection() (*gorm.DB, error) {
+
+	dbconn, err := gorm.Open("postgres",
+		"postgres://postgres:root@localhost/Note?sslmode=disable")
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %v", err)
+	}
+	return dbconn, nil
+}
