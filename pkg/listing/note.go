db037c794ce82a28a131f896a872a0a57c0d7a74
aleale2121 <alefewyimer4@gmail.com>
2021-03-08 15:02:37 +0300

    get note from json

--- pkg/listing/note.go
+++ pkg/listing/note.go
@@ -1,7 +1,17 @@
 package listing
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type Note struct {
 	ID     uint   `json:"id"`
 	Title  string `json:"title"`
 	Detail string `json:"detail"`
+}
+
+func (n *Note) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(n)
 }
